bf51df20b87255c6aeee20d073e40553a2f82fb2
Md Tausif <mdtausifofficial@gmail.com>
2023-12-25 00:52:48 +0530

    initial-commit
    working on resume portfoilio

---
+++ script.js
@@ -0,0 +1,10 @@
+// You can use JavaScript to fetch and display GitHub projects dynamically
+// For simplicity, you can use a static example here
+document.getElementById('github-projects').innerHTML = `
+  <a href="#" target="_blank">Project 1</a>
+  <a href="#" target="_blank">Project 2</a>
+  <a href="#" target="_blank">Project 3</a>
+  <a href="#" target="_blank">Project 4</a>
+  <a href="#" target="_blank">Project 5</a>
+  <a href="#" target="_blank">Project 6</a>
+`;
